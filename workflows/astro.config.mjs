ebe984b771e7038d96eed59a254e7c6ef95aa3cb
jaypolansky1 <jaypolansky@gmail.com>
2026-08-29 16:09:04 -0400

    Add Astro configuration file with dev options

---
+++ workflows/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // optional: set your production site URL here
+  // site: 'https://your-domain.example',
+  devOptions: {
+    port: 24
+  }
+});
