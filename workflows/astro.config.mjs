2ed32250603b8ce8f61700b8e679e1b58a153907
jaypolansky1 <jaypolansky@gmail.com>
2026-08-30 22:52:31 -0400

    Update site URL in astro.config.mjs

--- workflows/astro.config.mjs
+++ workflows/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://www.jaypolansky1.github.io/',
+  site: 'https://jaypolansky1.github.io/',
   base: '/job-finder-2',
 })
