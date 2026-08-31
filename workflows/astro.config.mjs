111939198fd0a77041be45750b2bc2f996994ad0
jaypolansky1 <jaypolansky@gmail.com>
2026-08-30 22:08:59 -0400

    Delete astro

--- workflows/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // optional: set your production site URL here
-  // site: 'https://your-domain.example',
-  devOptions: {
-    port: 24
-  }
-});
